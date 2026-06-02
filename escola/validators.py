d02787da9fac441d1f4800136bdf112aadf48ea2
Ewerton Dutra <ewerton-speed@hotmail.com>
2026-06-02 15:44:41 -0300

    adiciona validação para o formato do celular na função celular_invalido

--- escola/validators.py
+++ escola/validators.py
@@ -1,6 +1,12 @@
+import re
+
 def cpf_invalido(cpf):
     return len(cpf) != 11
+
 def nome_invalido(nome):
     return not nome.isalpha()
+
 def celular_invalido(celular):
-    return len(celular) != 13
+    # padrão esperado: 33 99999-8888 (13 caracteres, incluindo espaço e hífen)
+    padrao = '[0-9]{2} [0-9]{5}-[0-9]{4}'
+    return not re.findall(padrao, celular)
