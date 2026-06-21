98288888cf59753b6e2b75e30d4a31242eb2eee1
Ewerton Dutra <ewerton-speed@hotmail.com>
2026-06-21 17:05:52 -0300

    Explorando o swagger

--- escola/views.py
+++ escola/views.py
@@ -1,49 +1,118 @@
 from escola.models import Estudante, Curso, Matricula
-from escola.serializers import EstudanteSerializer, CursoSerializer, EstudanteSerializerV2, MatriculaSerializer, ListaMatriculasEstudanteSerializer, ListaMatriculasCursoSerializer
+from escola.serializers import (
+    EstudanteSerializer,
+    CursoSerializer,
+    EstudanteSerializerV2,
+    MatriculaSerializer,
+    ListaMatriculasEstudanteSerializer,
+    ListaMatriculasCursoSerializer,
+)
 from rest_framework import viewsets, generics, filters
 from rest_framework.permissions import AllowAny
 from escola.permissions import DjangoModelPermissionsWithView
 from django_filters.rest_framework import DjangoFilterBackend
 from escola.throttle import MatriculaUserRateThrottle
 
+
 class EstudanteViewSet(viewsets.ModelViewSet):
-    queryset = Estudante.objects.all().order_by('id')
-    permission_classes = [DjangoModelPermissionsWithView]
-    filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
-    ordering_fields = ['nome']
-    search_fields = ['nome', 'cpf']
+    """
+    Descrição da View:
+    - Gerencia os estudantes da escola.
+    Parâmetros:
+    - id (int): O identificador primário do estudante. Deve ser um número inteiro.
+    - nome (str): O nome do estudante. Deve ser uma string.
+    - email (str): O email do estudante. Deve ser uma string única e válida.
+    - cpf (str): O CPF do estudante. Deve ser uma string única.
+    - data_nascimento (date): A data de nascimento do estudante. Deve ser uma data válida.
+    - celular (str): O número de celular do estudante. Deve ser uma string.
+    """
     
+    queryset = Estudante.objects.all().order_by("id")
+    permission_classes = [DjangoModelPermissionsWithView]
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.OrderingFilter,
+        filters.SearchFilter,
+    ]
+    ordering_fields = ["nome"]
+    search_fields = ["nome", "cpf"]
+
     def get_serializer_class(self):
-        if self.request.version == 'v2':
+        if self.request.version == "v2":
             return EstudanteSerializerV2
         return EstudanteSerializer
 
+
 class CursoViewSet(viewsets.ModelViewSet):
-    queryset = Curso.objects.all().order_by('id')
+    """
+    Descrição da View:
+    - Gerencia os cursos disponíveis na escola.
+    Parâmetros:
+    - id (str): O identificador primário do curso. Deve ser uma string única.
+    - nome (str): O nome do curso. Deve ser uma string.
+    - nível (str): O nível do curso. Deve ser uma string, podendo ser "B" para Básico, "I" para Intermediário ou "A" para Avançado.
+    """
+
+    queryset = Curso.objects.all().order_by("id")
     serializer_class = CursoSerializer
     permission_classes = [DjangoModelPermissionsWithView]
 
+
 class MatriculaViewSet(viewsets.ModelViewSet):
-    queryset = Matricula.objects.all().order_by('id')
+    """
+    Descrição da View:
+    - Gerencia as matrículas dos estudantes nos cursos.
+    Parâmetros:
+    - id (int): O identificador primário da matrícula. Deve ser um número inteiro.
+    - estudante (int): O identificador do estudante associado à matrícula. Deve ser um número inteiro que corresponde a um estudante existente.
+    - curso (int): O identificador do curso associado à matrícula. Deve ser um número inteiro que corresponde a um curso existente.
+    - período (str): O período da matrícula. Deve ser uma string, podendo ser "M" para Matutino, "V" para Vespertino ou "N" para Noturno.
+    
+    Restrições:
+    - Um estudante não pode se matricular no mesmo curso mais de uma vez. A combinação de estudante e curso deve ser única.
+    
+    Trhottling:
+    - Limite de 5 requisições por minuto para cada usuário autenticado.
+    """
+
+    queryset = Matricula.objects.all().order_by("id")
     serializer_class = MatriculaSerializer
     throttle_classes = [MatriculaUserRateThrottle]
     permission_classes = [DjangoModelPermissionsWithView]
-    http_method_names = ['get', 'post']
-    
-class ListaMatriculaEstudante(generics.ListAPIView):    
-    def get_queryset(self):
-        queryset = Matricula.objects.filter(estudante__id=self.kwargs['pk'])
-        return queryset
-    
+    http_method_names = ["get", "post"]
+
+
+class ListaMatriculaEstudante(generics.ListAPIView):
+    """
+    Descrição da View:
+    - Lista Matriculas por id de Estudante
+    Parâmetros:
+    - pk (int): O identificador primário do objeto. Deve ser um número inteiro.
+    """
+
     serializer_class = ListaMatriculasEstudanteSerializer
     permission_classes = [DjangoModelPermissionsWithView]
 
+    def get_queryset(self):
+        if getattr(self, "swagger_fake_view", False):
+            return Matricula.objects.none()
+
+        return Matricula.objects.filter(estudante__id=self.kwargs["pk"])
+
 class ListaMatriculaCurso(generics.ListAPIView):
+    """
+    Descrição da View:
+    - Lista Matriculas por id de Curso
+    Parâmetros:
+    - pk (int): O identificador primário do objeto. Deve ser um número inteiro.
+    """
+
     authentication_classes = []
     permission_classes = [AllowAny]
-    
-    def get_queryset(self):
-        queryset = Matricula.objects.filter(curso__id=self.kwargs['pk'])
-        return queryset
-    
     serializer_class = ListaMatriculasCursoSerializer
+
+    def get_queryset(self):
+        if getattr(self, "swagger_fake_view", False):
+            return Matricula.objects.none()
+
+        return Matricula.objects.filter(curso__id=self.kwargs["pk"])
--- setup/urls.py
+++ setup/urls.py
@@ -2,6 +2,20 @@ from django.contrib import admin
 from django.urls import path, include
 from escola.views import EstudanteViewSet, CursoViewSet, MatriculaViewSet, ListaMatriculaEstudante, ListaMatriculaCurso
 from rest_framework.routers import DefaultRouter
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Documentação da API Escola",
+      default_version='v1',
+      description="Documentação da API para gerenciamento de estudantes, cursos e matrículas.",
+      terms_of_service="https://www.evolui.dev/termos/",
+      contact=openapi.Contact(email="contato@evolui.dev"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+)
 
 router = DefaultRouter()
 router.register('estudantes', EstudanteViewSet, basename='Estudantes')
@@ -13,4 +27,6 @@ urlpatterns = [
     path("<str:version>/", include(router.urls)),
     path('<str:version>/estudantes/<int:pk>/matriculas/', ListaMatriculaEstudante.as_view()),
     path('<str:version>/cursos/<int:pk>/matriculas/', ListaMatriculaCurso.as_view()),
+    path('<str:version>/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('<str:version>/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
