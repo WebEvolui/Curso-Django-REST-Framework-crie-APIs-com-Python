6dc16bc639ab8ee18b688399eb3eeef6fe06e655
Ewerton Dutra <ewerton-speed@hotmail.com>
2026-06-22 21:13:13 -0300

    test: add GET request authorization test for authenticated user

--- escola/tests/test_auth.py
+++ escola/tests/test_auth.py
@@ -1,10 +1,15 @@
 from django.contrib.auth.models import User
 from rest_framework.test import APITestCase
 from django.contrib.auth import authenticate
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import force_authenticate
 
 class AuthUserTestCase(APITestCase):
     def setUp(self):
         self.usuario = User.objects.create_superuser(username='admin', password='admin')
+        self.url = reverse('Estudantes-list', kwargs={'version': 'v1'})
+        
         
     def test_auth_user_ok(self):
         """Testa se o usuário é autenticado corretamente com as credenciais fornecidas."""
@@ -23,3 +28,10 @@ class AuthUserTestCase(APITestCase):
         
         user = authenticate(username='admin', password='invalidpassword')
         self.assertFalse((user is not None) and (user.is_authenticated))
+
+    def test_requisicao_get_autorizada(self):
+        """Testa se a requisição GET é autorizada para um usuário autenticado."""
+        
+        self.client.force_authenticate(user=self.usuario)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
